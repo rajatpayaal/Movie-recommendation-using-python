943a00c60e6d2e78bcfc48854cd41183fdd1bc73
Rajat <rajatpayal@outlook.in>
2021-01-08 22:07:59 +0530

    update

--- movie recomm.py
+++ movie recomm.py
@@ -1,22 +1,34 @@
 import pandas as pd
 import numpy as np
+# classification, regression and clustering algorithms including support vector machines
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-df = pd.read_csv("movie_dataset.csv")
+#read the data set file 
+df = pd.read_csv("movie_dataset.csv") 
+# differ... data in form of cast , genres , director 
 features = ['keywords','cast','genres','director']
+#row 
 def combine_features(row):
     return row['keywords'] +" "+row['cast']+" "+row["genres"]+" "+row["director"]
 for feature in features:
     df[feature] = df[feature].fillna('')
 df["combined_features"] = df.apply(combine_features,axis=1)
+# CountVectorizer() transform a given text into a vector
 cv = CountVectorizer()
 count_matrix = cv.fit_transform(df["combined_features"])
+#The cosine_sim matrix is a numpy array with calculated cosine similarity between each movies
 cosine_sim = cosine_similarity(count_matrix)
+
+#def get_title_from_index(index): return df[df.index == index]["title"].values[0]
+#  i=0 for movie in sorted_similar_movies: 
+#print(get_title_from_index(movie[0])) i=i+1 if i>15: break
 def get_title_from_index(index):
     return df[df.index == index]["title"].values[0]
 def get_index_from_title(title):
     return df[df.title == title]["index"].values[0]
+    #set movie avatar 
 movie_user_likes = "Avatar"
+
 movie_index = get_index_from_title(movie_user_likes)
 similar_movies =  list(enumerate(cosine_sim[movie_index]))
 sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)[1:]
